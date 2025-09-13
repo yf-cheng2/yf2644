fdc4897f14f9120167c4043fca9879f01760fe12
yf-cheng2 <yfkust@163.com>
2025-09-13 14:36:17 +0800

    Create hubconf.py

---
+++ hubconf.py
@@ -0,0 +1,7 @@
+dependencies = ['torch', 'math']
+def some_entry_fn(args, kwargs)
+    model = build_some_model(args, kwargs)
+    return model
+def another_entry_fn(args, kwargs)
+    model = build_another_model(args, kwargs)
+    return model
